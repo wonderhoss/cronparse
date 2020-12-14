7638f2bb5343555c29632f89a2f0824f5d0487db
Phil Taprogge <signup@lightweaver.info>
2020-12-14 11:58:30 +0000

    Handle version and help flags

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,29 @@ import (
 	"fmt"
 	"os"
 
+	flag "github.com/spf13/pflag"
+
 	"github.com/gargath/cronparse/pkg/cron"
 )
 
-func main() {
+var showVersion *bool = flag.Bool("version", false, "show version and exit")
+var showHelp *bool = flag.BoolP("help", "h", false, "show version and exit")
 
-	if os.Args[1] == "--version" {
+func main() {
+	flag.Parse()
+
+	if *showVersion {
 		fmt.Printf("cronparse %s\n\n", version())
 		os.Exit(0)
+	}
+
+	if *showHelp || len(os.Args) == 1 {
+		fmt.Printf("usage: %s <cron_expression>\n\n", os.Args[0])
+		flag.PrintDefaults()
+		if *showHelp {
+			os.Exit(0)
+		}
+		os.Exit(1)
 	}
 
 	e := cron.Expression{
